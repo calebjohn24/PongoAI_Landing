31b7258f6a1e83b019aceac2d3b89b64a2ddb500
Caleb John <cajohn0205@gmail.com>
2021-07-07 13:35:41 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# PongoAI Landing Page
+
+## Built with react and TailwindCSS
---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      spacing: {
+        128: "32rem",
+      },
+      width: {
+        "1/7": "14.2857143%",
+        "2/7": "28.5714286%",
+        "3/7": "42.8571429%",
+        "4/7": "57.1428571%",
+        "5/7": "71.4285714%",
+        "6/7": "85.7142857%",
+      },
+      height: {
+        "25vh": "25vh",
+        "35vh": "35vh",
+        "50vh": "50vh",
+        "75vh": "75vh",
+        "85vh": "85vh",
+        "90vh": "90vh",
+        "75":"18.75rem",
+        "38":"9.5rem"
+      }
+    },
+    fontFamily: {
+      sans: ["Ubuntu", "ui-sans-serif", "system-ui"],
+      serif: ["Ubuntu", "ui-serif", "Georgia"],
+      mono: ["Ubuntu Mono", "ui-monospace", "SFMono-Regular"],
+    },
+    
+    
+  },
+  variants: {
+    extend: {},
+  }
+};
